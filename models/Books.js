9de58bedb00cd0b70d7810c48793f81c238f3670
Jeftar Mascarenhas <jeftarmascarenhas@hotmail.com>
2017-05-14 13:36:04 -0300

    Iniciando configurações de ambiente, teste e modulos

---
+++ models/Books.js
@@ -0,0 +1,18 @@
+export default (sequelize, DataTypes) => {
+  const Books = sequelize.define('Books', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true
+      }
+    }
+  });
+
+  return Books;
+}
